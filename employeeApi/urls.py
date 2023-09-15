b3b12f61a46f5cff68861055d3035b19f95cfc57
Pundits-iTech <mohammedaalli088@gmail.com>
2023-09-15 11:58:46 +0000

    restful api with django and postgreSQL

---
+++ employeeApi/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import viewsets
+
+
+urlpatterns = [
+    path('api/employee/<str:fullname>/',viewsets.EmployeeViewset.as_view(), name='employee-fullname'),
+    path('api/employee/<int:id/',viewsets.EmployeeViewset.as_view(), name='employee-id'),
+]
--- employeeApi/viewsets.py
+++ employeeApi/viewsets.py
@@ -1,28 +1,30 @@
-from rest_framework import viewsets,filters
-from . import models
+from rest_framework import viewsets,status
+from .models import Employee
+from rest_framework.response import Response
 from . import serializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 
 class EmployeeViewset(viewsets.ModelViewSet):
-    queryset = models.Employee.objects.all()
+    queryset = Employee.objects.all()
     serializer_class = serializer.EmployeeSerializer
     # filter_backends = [DjangoFilterBackend, SearchFilter]
     # filterset_fields = ['fullname',]
 
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        id = self.request.query_params.get('id', None)
-        fullname = self.request.query_params.get('fullname', None)
+    def get_object(self):
+        param = self.kwargs.get('fullname')  # Get the URL parameter (fullname or id)
 
-        if id:
-            # Filter by ID if 'id' parameter is provided
-            queryset = queryset.filter(id=id)
-
-        if fullname:
-            # Filter by full name if 'fullname' parameter is provided
-            queryset = queryset.filter(fullname__icontains=fullname)
-
-        return queryset
+        try:
+            if param.isdigit():
+                # If the parameter is a digit, assume it's an ID
+                return Employee.objects.get(id=param)
+            else:
+                # Otherwise, assume it's a fullname
+                return Employee.objects.get(fullname=param)
+        except Employee.DoesNotExist:
+            return Response(
+                {"detail": "Employee not found."},
+                status=status.HTTP_404_NOT_FOUND
+            )
 

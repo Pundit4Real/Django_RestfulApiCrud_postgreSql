d8f531affef9ed6a815578037ef812a9649365e1
Pundits-iTech <mohammedaalli088@gmail.com>
2023-09-15 11:46:19 +0000

    restful api with django and postgreSQL

--- employeeApi/models.py
+++ employeeApi/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class Employee(models.Model):
     fullname = models.CharField(max_length=100)
-    emp_code = models.CharField(max_length=5)
+    emp_code = models.CharField(max_length=5,unique=True)
     address = models.CharField(max_length=50)
-    mobile = models.CharField(max_length=13)
+    mobile = models.CharField(max_length=13, unique=True)
--- employeeApi/viewsets.py
+++ employeeApi/viewsets.py
@@ -7,5 +7,22 @@ from rest_framework.filters import SearchFilter
 class EmployeeViewset(viewsets.ModelViewSet):
     queryset = models.Employee.objects.all()
     serializer_class = serializer.EmployeeSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
-    filterset_fields = ['fullname',]
+    # filter_backends = [DjangoFilterBackend, SearchFilter]
+    # filterset_fields = ['fullname',]
+
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        id = self.request.query_params.get('id', None)
+        fullname = self.request.query_params.get('fullname', None)
+
+        if id:
+            # Filter by ID if 'id' parameter is provided
+            queryset = queryset.filter(id=id)
+
+        if fullname:
+            # Filter by full name if 'fullname' parameter is provided
+            queryset = queryset.filter(fullname__icontains=fullname)
+
+        return queryset
+

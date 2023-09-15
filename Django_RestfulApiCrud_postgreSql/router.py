a42cccdf8d85602b8e306e958e69451a2016f992
Pundits-iTech <mohammedaalli088@gmail.com>
2023-09-15 08:40:06 +0000

    restful api with django and postgreSQL

---
+++ Django_RestfulApiCrud_postgreSql/router.py
@@ -0,0 +1,8 @@
+from employeeApi.viewsets import EmployeeViewset
+from rest_framework import routers
+
+# setting up the router function
+
+router = routers.DefaultRouter()
+router.register('employee',EmployeeViewset)
+
